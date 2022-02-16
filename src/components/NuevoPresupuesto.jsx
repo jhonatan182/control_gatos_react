3c567d6a20d2e10e687f6146271c92b8a52bb233
jhonatan182 <jona.vand98@gmail.com>
2022-02-16 16:02:58 -0600

    validadcion de formulario y nuevo componente de Mensaje

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,40 @@
-const NuevoPresupuesto = () => {
+import { useState } from "react";
+import Mensaje from './Mensaje';
+
+const NuevoPresupuesto = ({presupuesto,setPresupuesto}) => {
+
+    const [mensaje , setMensaje] = useState('');
+
+    const handleFormulario = e => {
+        e.preventDefault();
+
+        if( !Number(presupuesto) || Number(presupuesto) < 0 ) {
+            setMensaje('El presupuesto no es valido');
+
+        } else {
+            setMensaje('Es un presupuesto valido');
+        }
+    }
 
     return (
         <div className="contenedor-presupuesto contenedor sombra">
-            <form className="formulario">
+            <form  onSubmit={handleFormulario} className="formulario" >
                 <div className="campo">
                     <label htmlFor="">Definir Presupesto</label>
                     <input 
                         className="nuevo-presupuesto"
                         type="text"
                         placeholder="Añade tu presupuesto"
+                        value={presupuesto}
+                        onChange = { e => setPresupuesto(e.target.value) }
                         />
                     <input type="submit" value="Añadir"/>
                 </div>
+                
+                {mensaje && <Mensaje tipo='error'> {mensaje} </Mensaje>}
             </form>
+
+            
         </div>
     )
 }
